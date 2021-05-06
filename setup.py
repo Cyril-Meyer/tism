20834a571b4c2b7120fc6a5848f437a12ed0e94c
Cyril-Meyer <contact@cyrilmeyer.eu>
2021-05-06 16:18:06 +0200

    create setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+requirements = []
+
+setuptools.setup(
+    name="tism",
+    version="1.0.0",
+    author="Cyril Meyer",
+    author_email="contact@cyrilmeyer.eu",
+    description="TensorFlow Image Segmentation Models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cyril-Meyer/tism",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Operating System :: OS Independent",
+    ],
+)
